12677f78d09a96d5144466251e796849479c62ba
DominikaPieRep <dompie6@st.amu.edu.pl>
2018-08-06 11:52:01 +0200

    add superadmin model devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :superadmins
+  devise_for :siperadmins
+  devise_for :super_admins
   devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
    root to: "home#index"
