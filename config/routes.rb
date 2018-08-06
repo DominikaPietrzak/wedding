b4f973c47d6760b9833e8d696a0da93c6a557bf7
DominikaPieRep <dompie6@st.amu.edu.pl>
2018-08-06 15:06:08 +0200

    remove unnessery models with wrong names , add gem admin , remove unnesesery routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :guests
   devise_for :superadmins
-  devise_for :siperadmins
-  devise_for :super_admins
   devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
    root to: "home#index"
