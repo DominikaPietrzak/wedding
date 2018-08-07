114a62ccb4a4febe666b323a6716c55b341a09f8
DominikaPieRep <dompie6@st.amu.edu.pl>
2018-08-07 12:48:17 +0200

    add event references to paricipation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180807104424) do
+ActiveRecord::Schema.define(version: 20180807104648) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -101,6 +101,8 @@ ActiveRecord::Schema.define(version: 20180807104424) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "guest_id"
+    t.bigint "event_id"
+    t.index ["event_id"], name: "index_participations_on_event_id"
     t.index ["guest_id"], name: "index_participations_on_guest_id"
   end
 
@@ -121,5 +123,6 @@ ActiveRecord::Schema.define(version: 20180807104424) do
     t.index ["reset_password_token"], name: "index_superadmins_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "participations", "events"
   add_foreign_key "participations", "guests"
 end
