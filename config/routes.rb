84b4a6495ca5df0dda5a5b6244ac6bb9cb81f783
DominikaPieRep <dompie6@st.amu.edu.pl>
2018-08-07 17:09:46 +0200

    add active admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   devise_for :superadmins
   devise_for :user_admins
   devise_for :guests
