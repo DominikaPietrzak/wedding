3ecebfad61beafe0218961c562cd928139fd5ba6
DominikaPieRep <dompie6@st.amu.edu.pl>
2018-08-20 11:30:48 +0200

    add controllers to expense services

--- app/controllers/expense_services_controller.rb
+++ app/controllers/expense_services_controller.rb
@@ -1,2 +1,31 @@
 class ExpenseServicesController < ApplicationController
+
+before_action :authenticate_user!
+
+def index
+  @expense_services = current_user.expense_services.all
+end
+
+def new
+  @expense_service = current_user.expense_services.build
+end
+
+def create
+  @expense_service = current_user.expense_service.build(expense_services_params)
+end
+
+def update
+
+end
+
+def delete
+
+end
+
+private
+
+def expense_services_params
+  params.require(:expense_service).permit(:name, :cost, :paydeadline, :max_cost, :min_cost, :mid_cost, :max_amount, :mid_amount, :min_amount, :amount)
+end
+
 end
