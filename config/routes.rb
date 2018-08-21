3160de8db99522b90c3222af90abd006456eabcf
DominikaPieRep <dompie6@st.amu.edu.pl>
2018-08-21 11:16:40 +0200

    remove routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,6 @@ Rails.application.routes.draw do
   devise_for :guests
   resources :expense_services
   resources :expense_stuffs
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-   root to: "home#index"
-   get 'expense/services', to: 'expense_services#index'
-   get 'guests/new_guest', to: 'guests#new_guest'
+  root to: "home#index"
+  get 'expense/services', to: 'expense_services#index'
 end
