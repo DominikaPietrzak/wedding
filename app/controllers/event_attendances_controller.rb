f7c0823b506e1d84be46a3ddaa640d62553283a5
DominikaPieRep <dompie6@st.amu.edu.pl>
2018-08-30 14:47:44 +0200

    add to form field with guest id and event id

--- app/controllers/event_attendances_controller.rb
+++ app/controllers/event_attendances_controller.rb
@@ -4,11 +4,17 @@ class EventAttendancesController < ApplicationController
   def new
     @event_attendance = current_admin_user.event_attendances.build
     @events = current_admin_user.events.all
+    @guests = current_admin_user.guests.all
   end
 
   def create
-    @event_attendance = current_admin_user.event_attendances.build
+    
+    @event_attendance = current_admin_user.event_attendances.build(event_attendance_params)
+
     @event_attendance.save
   end
 
+  def event_attendance_params
+    params.require(:event_attendance).permit(:guest_id, :event_id, :status)
+  end
 end
