6ee9ae910290369de4c3bf7eebdfca640cbeb688
DominikaPieRep <dompie6@st.amu.edu.pl>
2018-09-04 09:48:22 +0200

    code refactor

--- app/controllers/event_attendances_controller.rb
+++ app/controllers/event_attendances_controller.rb
@@ -3,31 +3,14 @@ class EventAttendancesController < ApplicationController
   before_action :authenticate_admin_user!
 
   def create
-
     @event_attendance = current_admin_user.event_attendances.build(event_attendance_params)
     @event_attendance.save
-  end
-
-  def update
-    @guests = Guest.all
-    @events = Event.all
-
-    @event_attendance_statuses = EventAttendanceStatus.all
-    @event_attendance = current_admin_user.event_attendances.find(params[:id])
-    @event_attendance.update_attributes(event_attendance_params)
-    redirect_to update_event_attendance_path
-  end
-
-  def edit
-    @guests = Guest.all
-    @events = Event.all
-    @event_attendance_statuses = EventAttendanceStatus.all
-    @event_attendance = current_admin_user.event_attendances.find(params[:id])
   end
 
   def edit_event_attendance
     @guests = Guest.all
     @events = Event.all
+    @event_attendance_statuses = EventAttendanceStatus.all
   end
 
   def index
@@ -38,16 +21,11 @@ class EventAttendancesController < ApplicationController
 
 
   def update_event_attendance
-
     @guests = Guest.all
     @events = Event.all
     @event_attendance_statuses = EventAttendanceStatus.all
     @event_attendance = EventAttendance.find_by(id: params[:event_attendance][:event_attendance_id])
     @event_attendance.update_attributes(event_attendance_params)
-  end
-
-
-  def show
   end
 
   def event_attendance_params
