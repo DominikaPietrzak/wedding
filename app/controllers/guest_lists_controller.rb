0e9fe94b1a79ea0e6eefb0cc79050ce09e1e6ae3
DominikaPieRep <dompie6@st.amu.edu.pl>
2018-09-04 17:54:52 +0200

    end guest list update

--- app/controllers/guest_lists_controller.rb
+++ app/controllers/guest_lists_controller.rb
@@ -9,9 +9,21 @@ class GuestListsController < ApplicationController
   end
 
   def update_guest_list
-
+    @guests = Guest.all
+    @newlyweds = Newlywed.all
+    @guest_list = GuestList.find_by(id: params[:guest_list][:guest_list_id])
+    @guest_list.update_attributes(guest_list_params)
   end
 
   def edit_guest_list
+    @guests = Guest.all
+    @newlyweds = Newlywed.all
+
+    # @guest_list = GuestList.find_by(id: params[:guest_list][:guest_list_id])
   end
+
+  def guest_list_params
+    params.require(:guest_list).permit(:guest_id, :newlywed_id, :guest_list_status_id)
+  end
+
 end
