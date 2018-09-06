c729c19d22d058961374167266b0756943883d20
DominikaPieRep <dompie6@st.amu.edu.pl>
2018-09-06 11:36:18 +0200

    add accomapnig person view

--- app/controllers/accompaning_people_controller.rb
+++ app/controllers/accompaning_people_controller.rb
@@ -1,11 +1,17 @@
 class AccompaningPeopleController < ApplicationController
 
 def update_accompaning_people
-
+  @accompaning_person = AccompaningPerson.find_by(id: params[:accompaning_person][:id])
+  @accompaning_person.update_attributes(accompaning_people_params)
 end
 
 def edit_accompaning_people
   @guests = Guest.all
+  @accompaning_persons_number = AccompaningPersonsNumber.all
+end
+
+def accompaning_people_params
+  params.require(:accompanig_person).permit(:accomapnig_persons_number_id)
 end
 
 end
