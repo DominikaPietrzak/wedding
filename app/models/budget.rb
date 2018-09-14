6f239cbfaf66030d5c986eae05ddf811381c72e6
DominikaPieRep <dompie6@st.amu.edu.pl>
2018-09-14 12:14:15 +0200

    add budget functions

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -2,21 +2,26 @@ class Budget < ApplicationRecord
   belongs_to :admin_user
   belongs_to :event
 
-  def self.how_much_money_left_max(budget_event)
-    expense_service_cost = ExpenseService.sum_expense_service_cost
-    expense_stuff_cost = ExpenseStuff.sum_expense_stuff_cost
-    expense_sum = expense_service_cost + expense_stuff_cost
-    money_left = budget - expense_sum
+  def self.how_much_money_left_max(budget)
+    expense_cost = Expense.sum_expense_cost
+    money_left = budget.max - expense_sum
     if (money_left < 0)
-      puts "Przekroczyłeś budżet maxymalny o #{money_left} zł"
+      puts "Przekroczyłeś budżet maksymalny o #{money_left} zł"
     end
     if (money_left > 0)
-      puts "Jesteś #{money_left} powyżej budżetu mazymalnego"
+      puts "Jesteś #{money_left} zł powyżej budżetumaksymalnego"
     end
   end
 
-  def self.how_much_money_left_min
-
+  def self.how_much_money_left_min(budget)
+    expense_cost = Expense.sum_expense_cost
+    money_left = budget.min - expense_sum
+    if (money_left < 0)
+      puts "Przekroczyłeś budżet minimalny o #{money_left} zł"
+    end
+    if (money_left > 0)
+      puts "Jesteś #{money_left} zł powyżej budżetu minimalnego"
+    end
   end
 
 end
--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,7 +1,7 @@
 class Expense < ApplicationRecord
   belongs_to :admin_user
 
-  def self.sum_expense_service_cost
+  def self.sum_expense_cost
     Expense.pluck('sum((amount) *(cost))')
   end
 end
