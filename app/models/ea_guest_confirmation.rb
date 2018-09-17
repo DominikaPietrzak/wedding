a5b2b287082b814fcc02ec1325ad45ffa2ab92fa
DominikaPieRep <dompie6@st.amu.edu.pl>
2018-09-17 13:30:22 +0200

    fix bugs

--- app/models/ea_guest_confirmation.rb
+++ app/models/ea_guest_confirmation.rb
@@ -3,22 +3,4 @@ class EaGuestConfirmation < ApplicationRecord
   belongs_to :guest_confirmation_status
   belongs_to :event
 
-  def  self.create_or_delete_ea_guest_confirmation(event_attendance_object)
-    status_id = event_attendance_object.event_attendance_status_id
-    status = EventAttendanceStatus.find_by(id: status_id)
-
-    if (status.eastatus =="yes")
-      no_status = GuestConfirmationStatus.find_by(status: "no")
-      ea_confirmation = EaGuestConfirmation.find_by(guest_id: event_attendance_object.guest_id, event_id: event_attendance_object.event_id )
-      if (ea_confirmation == nil)
-        EaGuestConfirmation.create([{guest_id:event_attendance_object.guest_id, event_id: event_attendance_object.event_id, guest_confirmation_status_id: no_status.id}])
-      end
-    end
-    if (status.eastatus == "no")
-      eaguest_confirmation = EaGuestConfirmation.find_by(guest_id: event_attendance_object.guest_id, event_id: event_attendance_object.event_id)
-      if eaguest_confirmation
-        eaguest_confirmation.destroy
-      end
-    end
-  end
 end
--- app/models/event_attendance.rb
+++ app/models/event_attendance.rb
@@ -12,11 +12,17 @@ class EventAttendance < ApplicationRecord
     @events = Event.all
     @guests.each do |guest|
       @events.each do |event|
-        @event_attendance = EventAttendance.new
-        @event_attendance.guest_id = guest.id
-        @event_attendance.event_id = event.id
-        @event_attendance.admin_user_id = 1
-        @event_attendance.save
+        event_attendance = EventAttendance.new
+        event_attendance.guest_id = guest.id
+        event_attendance.event_id = event.id
+        event_attendance.admin_user_id = 1
+        event_attendance.save
+
+        ea_guest_confirmation = EaGuestConfirmation.new
+        ea_guest_confirmation.guest_id = guest.id
+        ea_guest_confirmation.event_id = event.id
+        ea_guest_confirmation.guest_confirmation_status_id = 1
+        ea_guest_confirmation.save
       end
     end
   end
