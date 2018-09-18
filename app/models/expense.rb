a4ac20e10408c064224d55907d34e115bed43d95
DominikaPieRep <dompie6@st.amu.edu.pl>
2018-09-18 10:15:00 +0200

    remove models

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,5 +1,7 @@
 class Expense < ApplicationRecord
   belongs_to :admin_user
+  has_one :expese_who
+  has_one :expense_event
 
   def self.sum_expense_cost
     Expense.pluck('sum((amount) *(cost))')
--- app/models/expense_event.rb
+++
@@ -1,4 +0,0 @@
-class ExpenseEvent < ApplicationRecord
-  belongs_to :expense
-  belongs_to :event
-end
