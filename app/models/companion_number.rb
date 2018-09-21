cc6ca52942096bdb163615c215cef7757002116c
DominikaPieRep <dompie6@st.amu.edu.pl>
2018-09-21 12:40:45 +0200

    add guest list newlywed with accompaning people

--- app/models/companion_number.rb
+++ app/models/companion_number.rb
@@ -2,7 +2,7 @@ class CompanionNumber < ApplicationRecord
   belongs_to :companion
   belongs_to :guest
 
-  def self.guest_sum_with_companions(event)
+  def self.event_guest_sum_with_companions(event)
     sum = 0
     yes_status_id = EventAttendanceStatus.find_by(eastatus: "yes").id
     attendances =  EventAttendance.where(event_id: event.id, event_attendance_status_id: yes_status_id)
@@ -15,4 +15,16 @@ class CompanionNumber < ApplicationRecord
     return sum
   end
 
+  def self.newlywed_guest_sum_with_companions(newlywed)
+    sum = 0
+    yes_status_id = GuestListStatus.find_by(status: "yes").id
+    lists =  GuestList.where(newlywed_id: newlywed.id, guest_list_status_id: yes_status_id)
+    lists.map do |list|
+      guests = Guest.where(id: list.guest_id)
+      guests.map do |guest|
+        sum = sum + guest.companion.number + 1
+      end
+    end
+    return sum
+  end
 end
