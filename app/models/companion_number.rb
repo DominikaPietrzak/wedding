b037accff57ee2ef46c7f49574f05c64da82452d
DominikaPieRep <dompie6@st.amu.edu.pl>
2018-09-21 11:31:16 +0200

    add function to sum guest with companions

--- app/models/companion_number.rb
+++ app/models/companion_number.rb
@@ -2,7 +2,17 @@ class CompanionNumber < ApplicationRecord
   belongs_to :companion
   belongs_to :guest
 
-  def self.guest_sum
-      CompanionNumber.sum(:number)
+  def self.guest_sum_with_companions(event)
+    sum = 0
+    yes_status_id = EventAttendanceStatus.find_by(eastatus: "yes").id
+    attendances =  EventAttendance.where(event_id: event.id, event_attendance_status_id: yes_status_id)
+    attendances.map do |att|
+      guests = Guest.where(id: att.guest_id)
+      guests.map do |guest|
+        sum = sum + guest.companion.number + 1
+      end
+    end
+    return sum
   end
+
 end
