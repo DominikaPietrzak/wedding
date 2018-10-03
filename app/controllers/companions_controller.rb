0b06df0d538b622efefa1b393ff5fa732d035915
DominikaPieRep <dompie6@st.amu.edu.pl>
2018-10-03 13:35:15 +0200

    remove comapnion number

--- app/controllers/companions_controller.rb
+++ app/controllers/companions_controller.rb
@@ -2,14 +2,14 @@ class CompanionsController < ApplicationController
 
 def update_accompaning_people
   @guests = Guest.all
-  @companion_numbers = CompanionNumber.all
+
   @companion = Companion.find_by(id: params[:companion][:companion_id])
   @companion.update_attributes(accompaning_people_params)
 end
 
 def edit_accompaning_people
   @guests = Guest.all
-  @companion_numbers = CompanionNumber.all
+
 end
 
 def accompaning_people_params
