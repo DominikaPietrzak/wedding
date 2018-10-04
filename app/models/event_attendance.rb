13e0eea3296ca31b65836385db19b741ffede970
DominikaPieRep <dompie6@st.amu.edu.pl>
2018-10-04 10:06:15 +0200

    fix create event attendance for event and guest

--- app/models/event_attendance.rb
+++ app/models/event_attendance.rb
@@ -10,19 +10,15 @@ class EventAttendance < ApplicationRecord
   def self.create_event_attendance_for_event_and_guest
     @guests = Guest.all
     @events = Event.all
+    no_status = EventAttendanceStatus.find_by(eastatus:"no").id
     @guests.each do |guest|
       @events.each do |event|
         event_attendance = EventAttendance.new
         event_attendance.guest_id = guest.id
         event_attendance.event_id = event.id
         event_attendance.admin_user_id = 1
+        event_attendance_status_id = no_status
         event_attendance.save
-
-        ea_guest_confirmation = EaGuestConfirmation.new
-        ea_guest_confirmation.guest_id = guest.id
-        ea_guest_confirmation.event_id = event.id
-        ea_guest_confirmation.guest_confirmation_status_id = 1
-        ea_guest_confirmation.save
       end
     end
   end
