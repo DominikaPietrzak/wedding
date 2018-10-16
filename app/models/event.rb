15978030f097b2440f26f3f663294e632e001dc5
DominikaPieRep <dompie6@st.amu.edu.pl>
2018-10-16 14:13:24 +0200

    make event delete working

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,10 +1,10 @@
 class Event < ApplicationRecord
-  has_many :event_attendances, dependent: :destroy
+  has_many :event_attendances, dependent: :delete_all
   belongs_to :admin_user
-  has_one :budget
-  has_many :expense
-  has_one :event_adress
-
+  has_one :budget, dependent: :destroy
+  has_many :expense, dependent: :destroy
+  has_one :event_adress, dependent: :destroy
+  has_many :ea_guest_confirmations, dependent: :destroy
   def self.create_event_attendance(event,admin)
     @guests = Guest.all
      @guests.each do |guest|
